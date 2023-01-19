b077d2838198864a44ca3f220cc72af83afa0ad2
Floe <i.a.spiridonova@gmail.com>
2023-01-19 16:27:30 +0300

    6

--- Fifth/Program.cs
+++ Fifth/Program.cs
@@ -1,27 +1,165 @@
-﻿Урок 5. Функции и одномерные массивы
-Задача 34: Задайте массив заполненный случайными положительными 
-трёхзначными числами. Напишите программу, которая покажет количество
-чётных чисел в массиве.
-[345, 897, 568, 234] -> 2
+﻿// Урок 5. Функции и одномерные массивы
+// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет количество
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
 
 
+// int Length = ReadInt("Введите размерность массива: ");
+// int [] numbers = new int[Length];
 
-Задача 36: Задайте одномерный массив, заполненный случайными числами.
-Найдите сумму элементов, стоящих на нечётных позициях.
-[3, 7, 23, 12] -> 19
-[-4, -6, 89, 6] -> 0
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+// int result = 0;
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] % 2 == 0)
+//     {
+//         result++;
+//     }
+// }
+// if (result % 10 == 1)
+// {
+//     Console.WriteLine($"В массиве {result} четное число");
+// }
+// if (result % 10 == 2 || result % 10 == 3 || result % 10 == 4)
+// {
+//     Console.WriteLine($"В массиве {result} четных числа");
+// }
+// else
+
+//     Console.WriteLine($"В массиве {result} четных чисел");
+
+// // Методы
+// void FillArrayRandomNumbers(int [] array) //Заполнение массива
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(99, 999);
+//     }
+// }
+
+// void PrintArray(int[] array) //Вывод массива на экран
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
 
 
+// int ReadInt(string message)  //Функция ввода
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
 
-Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
-максимальным и минимальным элементов массива.
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// int Length = ReadInt("Введите размерность массива: ");
+// int min = ReadInt("Введите минимальное число массива: ");
+// int max = ReadInt("Введите максимальное число массива: ");
+// int [] numbers = new int[Length];
+
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+// int result = 0;
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (i % 2 != 0)
+//     {
+//         result += numbers[i];
+//     }
+// }
+// Console.WriteLine($"Сумма элементов нечетных позиций = {result}");
+
+// // Методы
+// void FillArrayRandomNumbers(int [] array) //Заполнение массива
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(min, max);
+//     }
+// }
+
+// void PrintArray(int[] array) //Вывод массива на экран
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int ReadInt(string message)  //Функция ввода
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементов массива.
+
+int size = ReadInt("Введите размерность массива: ");
+int min = ReadInt("Введите минимальное число массива: ");
+int max = ReadInt("Введите максимальное число массива: ");
+int [] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int maxFillarray = numbers[0];
+int minFillarray = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > maxFillarray)
+    {
+        maxFillarray = numbers[i];
+    }
+    if (numbers[i] < minFillarray)
+    {
+        minFillarray = numbers[i];
+    }
+}
+Console.WriteLine($"Разница между максимальным и минимальным числом = {maxFillarray - minFillarray}");
 
 
+// Методы
+void FillArrayRandomNumbers(int [] array) //Заполнение массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
 
-Задача 30: - HARD необязательная Напишите программу, которая на вход
-получает размерность массива. Далее заполняет его случайными 
-уникальными числами и выводит на экран. Далее производим сортировку 
-массива от большего к меньшему и выводим на экран. Далее придумываем
-алгоритм перемешивания списка на основе случайности , применяем этот
-алгоритм и выводим на экран результат. Встроенные методы работы со списками
-использовать нельзя.
+void PrintArray(int[] array) //Вывод массива на экран
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)  //Функция ввода
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Задача 30: - HARD необязательная Напишите программу, которая на вход
+// получает размерность массива. Далее заполняет его случайными 
+// уникальными числами и выводит на экран. Далее производим сортировку 
+// массива от большего к меньшему и выводим на экран. Далее придумываем
+// алгоритм перемешивания списка на основе случайности , применяем этот
+// алгоритм и выводим на экран результат. Встроенные методы работы со списками
+// использовать нельзя.
