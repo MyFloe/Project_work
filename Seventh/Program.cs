170d09d8e7d4d31018b023503a20c070e317a533
Floe <i.a.spiridonova@gmail.com>
2023-01-20 15:37:53 +0300

    7

---
+++ Seventh/Program.cs
@@ -0,0 +1,143 @@
+﻿// Урок 7. Двумерные массивы
+// Задача 47. Задайте двумерный массив размером m×n, заполненный 
+// случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+// Console.WriteLine("Введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int cols = Convert.ToInt32(Console.ReadLine());
+// double [,] array = new double[rows,cols];
+// FillArray(array);
+// PrintArray(array);
+
+
+// void FillArray(double[,] array)
+// {
+//     for (int i=0;i<array.GetLength(0);i++)
+//         for (int j=0;j<array.GetLength(1);j++)
+//             array[i,j] = Convert.ToDouble(new Random().Next(-1000,1000))/10; 
+            
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i=0;i<array.GetLength(0);i++)
+//     {
+//         for (int j=0;j<array.GetLength(1);j++)
+//             Console.Write($"{array[i,j],3} \t");
+//         Console.WriteLine();
+//     }
+// }
+
+
+
+
+// Задача 50. Напишите программу, которая на вход принимает значение 
+// элемента в двумерном массиве, и возвращает позицию этого элемента 
+// или же указание, что такого элемента нет.
+
+// 17 -> такого числа в массиве нет
+
+// int rows = ReadInt("Введите индекс строки: ");
+// int colums = ReadInt("Введите индекс столбца: ");
+// int[,] numbers = new int[6, 8];
+// FillArray2D(numbers);
+// PrintArray2D(numbers);
+
+// if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine(numbers[rows, colums]);
+// else Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+
+
+// // Заполнение массива рандомными числами от 1 до 9
+// void FillArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// //  Функция вывода массива в терминал
+// void PrintArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// // Функция ввода 
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите 
+// среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Random random = new Random();
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("---------------------------");
+Console.WriteLine(arr.GetLength(0));
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0)} ");
+}
+Console.ReadLine();
+
+
+// Задача HARD SORT необязательная. Считается за три обязательных
+// Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 10 3
+
+// После сортировки
+// 1 2 3 4
+// 5 7 9 10
+
+// задача 2 HARD необязательная. Считается за четыре обязательных ) то есть 
+// три этих и одна с будущего семинара
+// Сгенерировать массив случайных целых чисел размерностью m*n 
+// (размерность вводим с клавиатуры) , причем чтоб количество элементов 
+// было четное. Вывести на экран красивенько таблицей. Перемешать случайным 
+// образом элементы массива, причем чтобы каждый элемент гарантированно и только 
+// один раз переместился на другое место и выполнить перемешивание за m*n / 2 итераций.
+//  То есть если массив три на четыре, то надо выполнить за 6 итераций. 
+//  И далее в конце опять вывести на экран как таблицу.
